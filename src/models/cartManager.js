f63701fb95fbc0e367fa626695704592068dddfa
Jesus <jesuscontell2020@gmail.com>
2024-01-20 17:34:19 +0100

    Arreglando cartManager

--- src/models/cartManager.js
+++ src/models/cartManager.js
@@ -1,6 +1,8 @@
 import { promises as fs } from "fs";
 import { v4 as uuidv4 } from 'uuid'
 
+const productsJson = "../products.json"
+
 export class CartManager {
 
   constructor(){
@@ -45,84 +47,214 @@ export class CartManager {
 
     const newCart = {
 
+        id: uuidv4,
         products: [],
 
     }
 
-    carts.id = uuidv4()
     carts.push(newCart)
     await fs.writeFile(this.file, JSON.stringify(carts))
     return newCart;
 
   }
 
+  //Delete
+
+  async deleteCart(Id) {
+
+    const cartId = (Id)
+
+    try {
+
+      const carts = await this.getCarts()
+      const cartIndex = carts.findIndex((cart) => cart.id === cartId)
+  
+      if (cartIndex === -1) {
+
+        console.log("El carrito no encontrado")
+        return false
+
+      } else {
+
+        carts.splice(cartIndex, 1)
+        await fs.writeFile(this.file, JSON.stringify(carts, null, 2))
+        return true;
+
+      }
+    } catch (error) {
+
+      console.log("Hubo un error")
+      return false;
+
+    }
+
+  }
 
   async addProduct(cartId, productId, quantity) {
 
     try {
 
-      const carts = await this.getCarts();
+      const carts = await this.getCarts()
+      const products = await this.getProducts()
 
-      const products = await this.getProducts();
+      const cartIndex = carts.findIndex(
 
-      const cartIn = carts.findIndex(
-
-        (cart) => parseInt(cart.id) === parseInt(cartId)
-
-      );
-
-      const product = products.find(
-
-        (prod) => parseInt(prod.id) === parseInt(productId)
-
-      );
-
-      if (cartIn === -1 || !product) {
-
-        console.log("No encontrado")
-
-        return false;
-      }
-
-      const existProd = carts[cartIn].products.find(
-
-        (prod) => prod.id === productId
+        (cart) => cart.id === cartId
 
       )
 
-      if (existProd) {
+      const product = products.find(
 
-        console.log("Existe el producto")
+        (prod) => prod.id === productId
 
-        existProd.quantity += parseInt(quantity)
+      );
 
+      if (cartIndex === -1 || !product) {
+
+        console.log("No se ha encontrado carrito ni producto")
+        return false;
+
+      }
+
+      const prod = carts[cartIndex].products.find(
+
+        (el) => el.id === productId)
+
+      if (prod) {
+
+        console.log("El producto existe")
+        prod.quantity += Number(quantity)
+         
       } else {
 
-        console.log("Producto agregado")
+        console.log("Agregar producto")
 
-        carts[cartIn].products.push({
+        carts[cartIndex].products.push({
 
-          id: uuidv4(),
+          id: productId,
           title: product.title,
           description: product.description,
           price: product.price,
           thumbnail: product.thumbnail,
           code: product.code,
-          quantity: parseInt(quantity),
+          quantity: Number(quantity),
 
         })
+
       }
 
+      await fs.writeFile(this.file, JSON.stringify(carts, null, 2))
+      return true
+
+    } catch (error) {
+
+      console.error("Error", error)
+      return false
+
+    }
+
+  }
+
+  async updateQuantity(cartId, productId, newQuantity) {
+
+    try {
+
+      const carts = await this.getCarts()
+
+      const cartIndex = carts.findIndex((cart) => cart.id === cartId)
+
+      if (cartIndex === -1) {
+
+        console.log("Carrito no existe.")
+        return false
+
+      }
+
+      const cart = carts[cartIndex]
+      const productIndex = cart.products.findIndex(
+
+        (prod) => prod.id === productId
+
+      )
+
+      if (productIndex === -1) {
+
+        console.log("Producto no encontrado.")
+        return false
+
+      }
+
+      cart.products[productIndex].quantity = parseInt(newQuantity)
       await fs.writeFile(this.file, JSON.stringify(carts, null, 2))
 
       return true
 
     } catch (error) {
 
-      console.error("Error", error);
+      console.error("Error al actualizar", error)
+      return false
 
-      return false;
     }
+
+  }
+
+  async removeProduct(cartId, productId) {
+
+    try {
+
+      const carts = await this.getCarts()
+      const cartIndex = carts.findIndex((cart) => cart.id === cartId)
+
+      if (cartIndex === -1) {
+
+        console.log("Error: No se ha encontrado")
+        return false
+
+      }
+
+      const cart = carts[cartIndex]
+
+      const productIndex = cart.products.findIndex(
+
+        (prod) => prod.id === productId
+
+      )
+
+      if (productIndex === -1) {
+
+        console.log("Producto no encontrado")
+        return false
+
+      }
+
+      cart.products.splice(productIndex, 1)
+      await fs.writeFile(this.file, JSON.stringify(carts, null, 2));
+      return true
+
+    } catch (error) {
+
+      console.error("Error", error)
+      return false
+
+    }
+
+  }
+
+  async getProducts() {
+
+    try {
+
+      const content = await fs.readFile(productsJson, "utf-8")
+      return JSON.parse(content)
+
+    } catch (error) {
+
+      console.error("Error en los productos.", error)
+      return []
+
+    }
+
   }
   
 }
+
